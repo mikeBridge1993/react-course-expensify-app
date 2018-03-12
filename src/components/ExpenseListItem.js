3ef8597945bfd4a968ddde1a30186d75353231eb
mikeBridge1993 <miguelponte93@hotmail.com>
2018-03-12 16:52:30 -0100

    Setup devDependencies and dist folder

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,22 +4,32 @@ import {removeExpense} from '../actions/expenses';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import moment from 'moment';
 
-const ExpenseListItem = (props) => {
+export class ExpenseListItem extends React.Component {
+  onRemove = () => {
+    this.props.removeExpense({id: this.props.id});
+  };
+
+ render () {
   return (
     <li className="list-group-item d-flex justify-content-between">
       <div className="d-flex ExpenseListItem rounded">
-          <span className="mr-auto text-secondary bold"><Link to={"/edit/" + props.id} >{props.description}</Link></span>
-          <span className=" badge badge-pill badge-success mx-2">{props.amount}<i className="fa fa-euro ml-1"></i></span>
-          <span className="badge badge-pill ml-auto badge-primary">{moment(props.createdAt).format("DD-MM-YYYY")}<i className="fa fa-calendar ml-1"></i></span>
+          <span className="mr-auto text-secondary bold"><Link to={"/edit/" + this.props.id} >{this.props.description}</Link></span>
+          <span className=" badge badge-pill badge-success mx-2">{this.props.amount}<i className="fa fa-euro ml-1"></i></span>
+          <span className="badge badge-pill ml-auto badge-primary">{moment(this.props.createdAt).format("DD-MM-YYYY")}<i className="fa fa-calendar ml-1"></i></span>
       </div>
       <div>
-      <button onClick={e => props.dispatch(removeExpense({id: props.id}))} className="delete-button  px-2 py-1"><i className="fa fa-times fa-1x delete-button--icon"></i></button>
+      <button onClick={this.onRemove} className="delete-button  px-2 py-1"><i className="fa fa-times fa-1x delete-button--icon"></i></button>
       </div>
     </li>
-  );
+    );
+  }
 }
 
-export default ExpenseListItem;
+const mapDispatchToProps = (dispatch, props) => ({
+  removeExpense: (data) => dispatch(removeExpense(data))
+});
+
+export default connect(undefined, mapDispatchToProps)(ExpenseListItem);
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, arg) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -31,7 +31,8 @@ module.exports = (env, arg) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 }
