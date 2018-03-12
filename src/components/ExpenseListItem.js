7c5eb192b25a36ca92b7212a38b795f0eaddecd3
mikeBridge1993 <miguelponte93@hotmail.com>
2018-03-12 18:24:27 -0100

    Currency format added

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import {removeExpense} from '../actions/expenses';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 export class ExpenseListItem extends React.Component {
   onRemove = () => {
@@ -14,7 +15,7 @@ export class ExpenseListItem extends React.Component {
     <li className="list-group-item d-flex justify-content-between">
       <div className="d-flex ExpenseListItem rounded">
           <span className="mr-auto text-secondary bold"><Link to={"/edit/" + this.props.id} >{this.props.description}</Link></span>
-          <span className=" badge badge-pill badge-success mx-2">{this.props.amount}<i className="fa fa-euro ml-1"></i></span>
+          <span className=" badge badge-pill badge-success mx-2">{numeral(this.props.amount/100).format("$0,0.00")}</span>
           <span className="badge badge-pill ml-auto badge-primary">{moment(this.props.createdAt).format("DD-MM-YYYY")}<i className="fa fa-calendar ml-1"></i></span>
       </div>
       <div>
