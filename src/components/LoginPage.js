5c29cf542073b67ef4f2feab3fbab88f462a2d09
mikeBridge1993 <miguelponte93@hotmail.com>
2018-03-15 20:05:28 -0100

    Styling in login page tweaked

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,11 +8,9 @@ export class LoginPage extends React.Component {
     return (
       <div className="login-box d-flex align-items-center">
         <div className="login-box__box mx-auto bg-dark pb-3 col-4 rounded">
-        <form className="form-signin">
           <h1 className="login-box__title text-center">X<span className="text-light">pensify</span></h1>
           <p className="lead text-center text-secondary">It's time to get your expenses in check.</p>
           <button onClick={this.props.startLogin} className="btn btn-lg btn-block col-6 offset-3 btn-login btn-primary my-4" type="submit">Sign in with <i className="fa fa-google text-danger"></i></button>
-        </form>
       </div> 
     </div>
     )
